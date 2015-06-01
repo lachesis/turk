d1708d5fa86a0c0aea198b17efcb08adfe5ffdbe
Richard Klafter <rpklafter@yahoo.com>
2015-05-31 17:37:37 -0700

    added getQueryVariable

--- code.js
+++ code.js
@@ -65,6 +65,19 @@ function getPoints(){
   return points;
 }
 
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+    return null;
+}
+
 function postPoints(){
   var vars = [], hash;
   var q = document.URL.split('?')[1];
